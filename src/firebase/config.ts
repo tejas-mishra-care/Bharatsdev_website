5eaa6ffe115a53af889ec6bca9255d3423e6d579
Tejas Mishra <tejas.mishra.care@gmail.com>
2026-05-04 03:53:44 +0530

    fix: switch to new Bharatsdev Firebase project

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyDtcfP-BZ1ZzUwPOk8uoFnNZt-WCsT84S0",
-  authDomain: "geoverseai-1.firebaseapp.com",
-  databaseURL: "https://geoverseai-1-default-rtdb.firebaseio.com",
-  projectId: "geoverseai-1",
-  storageBucket: "geoverseai-1.firebasestorage.app",
-  messagingSenderId: "96380169011",
-  appId: "1:96380169011:web:3dfca471112d6a7133ac46",
-  measurementId: "G-4BS11BN67D"
+  apiKey: "AIzaSyC_o5qeOPukYwRuxB0PCSKIASvhNQadays",
+  authDomain: "studio-1467262740-d714c.firebaseapp.com",
+  projectId: "studio-1467262740-d714c",
+  storageBucket: "studio-1467262740-d714c.firebasestorage.app",
+  messagingSenderId: "997645001091",
+  appId: "1:997645001091:web:81ca30920d60442a86d6b6"
 };
+
